ba17cabb29aa3ebaf4f026910c8ebe9df4ccadec
Linda <oneill.linda21@gmail.com>
2018-04-26 19:55:58 -0500

    add pundit gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  after_initialize { self.role ||= :standard }
+  before_save { self.email = email.downcase if email.present? }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+
+  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+  validates :password, length: { minimum: 6 }, allow_blank: true
+
+  validates :email,
+             presence: true,
+             uniqueness: { case_sensitive: false },
+             length: { minimum: 3, maximum: 254 }
+
+enum role: [:standard]
+
 end
