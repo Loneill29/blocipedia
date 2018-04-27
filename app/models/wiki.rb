f7e3d51a16107ade5f7c287868691359172e34fd
Linda <oneill.linda21@gmail.com>
2018-04-27 11:57:48 -0500

    add faker gem

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,6 +1,6 @@
 class Wiki < ApplicationRecord
-  belongs_to :user
+  #belongs_to :user
 
   validates :title, length: { minimum: 5 }, presence: true
-  validates :body, length: { minimum: 20 }, presence: true
+  validates :body, length: { minimum: 5 }, presence: true
 end
