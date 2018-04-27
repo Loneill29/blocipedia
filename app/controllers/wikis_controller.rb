1da90961f24e95a507c0d8dd7d244bb46f5ac89f
Linda <oneill.linda21@gmail.com>
2018-04-26 22:01:42 -0500

    add strong params

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -54,10 +54,7 @@ class WikisController < ApplicationController
   end
 
   private
-      # Using a private method to encapsulate the permissible parameters
-      # is just a good pattern since you'll be able to reuse the same
-      # permit list between create and update. Also, you can specialize
-      # this method with per-user checking of permissible attributes.
+
       def wiki_params
         params.require(:wiki).permit(:title, :body)
       end
