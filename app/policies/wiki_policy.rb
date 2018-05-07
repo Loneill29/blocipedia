3f4c3267e4052571c341a36e697f80e359737f80
Linda <oneill.linda21@gmail.com>
2018-05-07 18:41:04 -0500

    revised routes and wiki_policy

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -35,34 +35,34 @@ class WikiPolicy < ApplicationPolicy
   end
 
   class Scope
-  attr_reader :user, :scope
+     attr_reader :user, :scope
 
-  def initialize(user, scope)
-    @user = user
-    @scope = scope
-  end
+     def initialize(user, scope)
+       @user = user
+       @scope = scope
+     end
 
-  def resolve
-    wikis = []
-    if user.role == 'admin'
-      wikis = scope.all
-    elsif user.role == 'premium'
-      all_wikis = scope.all
-      all_wikis.each do |wiki|
-        if !wiki.private? || user = wiki.user_id || wiki.collaborators.include?(user)
-          wikis << wiki
-        end
-      end
-    else
-      all_wikis = scope.all
-      wikis = []
-      all_wikis.each do |wiki|
-        if !wiki.private? || wiki.collaborators.include?(user)
-          wikis << wiki
-        end
-      end
-    end
-    wikis
-  end
-end
-end
+     def resolve
+       wikis = []
+       if user.role == 'admin'
+         wikis = scope.all
+       elsif user.role == 'premium'
+         all_wikis = scope.all
+         all_wikis.each do |wiki|
+           if wiki.public? || wiki.owner == user || wiki.collaborators.include?(user)
+             wikis << wiki
+           end
+         end
+       else
+         all_wikis = scope.all
+         wikis = []
+         all_wikis.each do |wiki|
+           if wiki.public? || wiki.collaborators.include?(user)
+             wikis << wiki
+           end
+         end
+       end
+       wikis
+     end
+   end
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   resources :charges, only: [:new, :create, :destroy]
   resources :wikis
-  
+
   resources :wikis do
-      resources :collaborators, only: [:new, :create, :destroy]
+      resources :collaborators, only: [:create, :destroy]
   end
   devise_for :users
   get 'about' => 'welcome#about'
